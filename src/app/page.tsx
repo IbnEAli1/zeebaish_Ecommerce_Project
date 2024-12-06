5a9dbe9703859ab490cf01a99fe3295a0ccc0dc0
Syed Abbas Ai Zaidi <asabb625@gmail.com>
2024-12-07 02:36:36 +0500

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import Navbar from "./component/navbar";
+// import Pagetwo from "./components/Pagetwo";
+// import Header from "./components/Header";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar  />
-    </div>
+  return ( <div>   
+
+
+   </div>
   );
 }
